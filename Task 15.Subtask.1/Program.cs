23e51d71e4a8f3a0ceb25f047e3b4aa2368c8a0b
stank <stankevich_dg@mail.ru>
2023-01-21 17:15:46 +0400

    Добавьте файлы проекта.

---
+++ Task 15.Subtask.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+        var allStudents = GetAllStudents(classes);
+
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        var result = classes.SelectMany(x=>x.Students).ToArray();
+        return result;       
+    }
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
